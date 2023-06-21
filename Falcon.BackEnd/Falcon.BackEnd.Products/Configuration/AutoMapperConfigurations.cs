a88a2f21e0ddf323f849bb4435e12eaba312df39
atlronny8047 <ronny.harianto@ai.astra.co.id>
2023-06-21 16:07:37 +0700

    Clean Code

--- Falcon.BackEnd/Falcon.BackEnd.Products/Configuration/AutoMapperConfigurations.cs
+++ Falcon.BackEnd/Falcon.BackEnd.Products/Configuration/AutoMapperConfigurations.cs
@@ -6,24 +6,24 @@ using Falcon.Models.Topics;
 
 namespace Falcon.BackEnd.Products.Configuration
 {
-    public class AutoMapperConfigurations : Profile
-    {
-        public AutoMapperConfigurations()
-        {
-            CreateMap<ProductInput, Product>();
-            CreateMap<ProductVariantInput, ProductVariant>();
-            CreateMap<Product, ProductDto>();
-            CreateMap<ProductVariant, ProductVariantDto>();
-            CreateMap<ProductDto, ProductCreated>()
-                .ForMember(d => d.ProductId, o => o.MapFrom(s => s.Id));
+	public class AutoMapperConfigurations : Profile
+	{
+		public AutoMapperConfigurations()
+		{
+			CreateMap<ProductInput, Product>();
+			CreateMap<ProductVariantInput, ProductVariant>();
+			CreateMap<Product, ProductDto>();
+			CreateMap<ProductVariant, ProductVariantDto>();
+			CreateMap<ProductDto, ProductCreated>()
+				.ForMember(d => d.ProductId, o => o.MapFrom(s => s.Id));
 
 			CreateMap<ProductUpdate, Product>()
 				.ForMember(x => x.Code, opt => opt.Ignore());
 
-            CreateMap<VariantProductInput, ProductVariant>();
-            CreateMap<ProductVariant, VariantProductDto>();
-            CreateMap<VariantProductDto, ProductVariantCreated>()
-                .ForMember(d => d.ProductVariantId, o => o.MapFrom(s => s.Id));
-        }
-    }
+			CreateMap<VariantProductInput, ProductVariant>();
+			CreateMap<ProductVariant, VariantProductDto>();
+			CreateMap<VariantProductDto, ProductVariantCreated>()
+				.ForMember(d => d.ProductVariantId, o => o.MapFrom(s => s.Id));
+		}
+	}
 }
--- Falcon.BackEnd/Falcon.BackEnd.Products/Program.cs
+++ Falcon.BackEnd/Falcon.BackEnd.Products/Program.cs
@@ -1,8 +1,6 @@
-using Falcon.BackEnd.Products.Controllers.Products.CustomModels;
 using Falcon.BackEnd.Products.Domain;
 using Falcon.BackEnd.Products.Service.Products;
 using Falcon.Libraries.Microservice.Startups;
-using Microsoft.Extensions.Configuration;
 
 var app = new Startup<ApplicationDbContext>(args);
 
