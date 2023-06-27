406c56df35ec0b036850041b1791a546c22066ca
ibnusadam <37340754+ibnusadam@users.noreply.github.com>
2023-06-27 11:48:05 +0700

    Data Dami NotificationTemplate Dan UserNotif
    Data Dami NotificationTemplate Dan UserNotif

--- Falcon.BackEnd/Falcon.BackEnd.Notifications/Domain/Models/Builders/NotificationTemplateEntityBuilder.cs
+++ Falcon.BackEnd/Falcon.BackEnd.Notifications/Domain/Models/Builders/NotificationTemplateEntityBuilder.cs
@@ -19,6 +19,84 @@ namespace Falcon.BackEnd.Notifications.Domain.Models.Builders
 
             builder
                 .Property(e => e.Code);
+
+            DataSeeding(builder);
+        }
+
+        private static void DataSeeding(EntityTypeBuilder<NotificationTemplate> builder)
+        {
+            builder
+                .HasData(new NotificationTemplate()
+                {
+                    Id = Guid.NewGuid(),
+                    Title = "Version",
+                    Body = "Version Terbaru Tersedia",
+                    Code = "UpgradeVersion"
+
+                });
+
+            builder
+                .HasData(new NotificationTemplate()
+                {
+                    Id = Guid.NewGuid(),
+                    Title = "Partner",
+                    Body = "Sudah Di Approve",
+                    Code = "ApprovePartner"
+                });
+
+            builder
+                .HasData(new NotificationTemplate()
+                {
+                    Id = Guid.NewGuid(),
+                    Title = "Partner",
+                    Body = "Telah Di Reject",
+                    Code = "RejectPartner"
+                });
+
+            builder
+                .HasData(new NotificationTemplate()
+                {
+                    Id = Guid.NewGuid(),
+                    Title = "Visit Plan",
+                    Body = "Sudah Di Approve",
+                    Code = "ApproveVisitPlan"
+                });
+
+            builder
+                .HasData(new NotificationTemplate()
+                {
+                    Id = Guid.NewGuid(),
+                    Title = "Visit Plan",
+                    Body = "Telah Di Reject",
+                    Code = "RejectVisitPlan"
+                });
+
+            builder
+                .HasData(new NotificationTemplate()
+                {
+                    Id = Guid.NewGuid(),
+                    Title = "POA",
+                    Body = "Sudah Di Approve",
+                    Code = "ApprovePOA"
+                });
+
+            builder
+                .HasData(new NotificationTemplate()
+                {
+                    Id = Guid.NewGuid(),
+                    Title = "POA",
+                    Body = "Telah Di Reject",
+                    Code = "RejectPOA"
+                });
+
+            builder
+                .HasData(new NotificationTemplate()
+                {
+                    Id = Guid.NewGuid(),
+                    Title = "Outlet",
+                    Body = "Request Approval",
+                    Code = "CreateOutlet"
+                });
         }
     }
 }
